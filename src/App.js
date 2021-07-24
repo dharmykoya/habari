5e46cadf4147130b06bc7cf554cb311872b931af
Damilola Adekoya <dharmykoya38@gmail.com>
2021-07-24 23:38:28 +0100

    feature(landing-page):landing-page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Homepage from "./views/homepage/Homepage";
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Homepage />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/assets/icons/ArrowRightIcon.js
@@ -0,0 +1,16 @@
+const ArrowRightIcon = () => (
+  <svg
+    width="18"
+    height="18"
+    viewBox="0 0 18 18"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M3 9C3 5.6925 5.6925 3 9 3C12.3075 3 15 5.6925 15 9C15 12.3075 12.3075 15 9 15C5.6925 15 3 12.3075 3 9ZM1.5 9C1.5 13.14 4.86 16.5 9 16.5C13.14 16.5 16.5 13.14 16.5 9C16.5 4.86 13.14 1.5 9 1.5C4.86 1.5 1.5 4.86 1.5 9ZM9 8.25H6L6 9.75L9 9.75V12L12 9L9 6V8.25Z"
+      fill="#0077FF"
+    />
+  </svg>
+);
+
+export default ArrowRightIcon;
